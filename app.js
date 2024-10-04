3fc08aff6f97e12f1c9d4c4d7c6bae6d652bf8b0
MatsWilliam <mats.blomqvist@hotmail.com>
2024-10-04 12:09:45 +0200

    JS arrayer och objekt

---
+++ app.js
@@ -0,0 +1,20 @@
+// En array där alla monsterobjekt ska landa
+const allMonsters= [];
+
+
+// Objekt som visar egenskaperna som monstren har
+const monster ={
+    name: "Demo",
+    type: "Ice",
+    colour: "blue",
+    heads: 1,
+    tails: 0,
+    arms: 4,
+    horns:2,
+};
+
+// Array med typerna
+const types = ["Ismonster", "Eldmonster", "Blixtmonster"];
+// Array med färgerna
+const colours = ["Röd", "Blå", "Gul", "Grön", "Lila"];
+
