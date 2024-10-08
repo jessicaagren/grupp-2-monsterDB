e994f29f0f4f93c6af32e9c02bdd2b197baf1979
SiriJV <siri.viren@icloud.com>
2024-10-08 11:54:32 +0200

    Lagt till färgval för radiobuttons

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ const monster = {
 
 // Array med typerna
 const types = ["Ismonster", "Eldmonster", "Blixtmonster"];
-// Array med färgerna
-const colours = ["Röd", "Blå", "Gul", "Grön", "Lila"];
 
 const typeSelect = document.querySelector("#monsterType");
 
@@ -26,3 +24,26 @@ for (const type of types) {
     typeOption.value = type;
     typeSelect.appendChild(typeOption);
 }
+
+// array med färgerna
+const monsterColours = ["röd", "rosa", "blå", "grön", "gul"];
+
+const labelElement = document.querySelector('#monsterColour');
+
+// loopar igenom arrayen med färger och skapar radio input och korresponderande labels
+for (const el of monsterColours) {
+    const colourOptions = document.createElement("input");
+    colourOptions.type = "radio"; // gör alla till radio buttons
+    colourOptions.value = el; // ger unikt value som motsvarar färgnamnen
+    colourOptions.name = "colour"; // grupperar alla radio buttons genom gemensamt namn
+    colourOptions.id = el; // ger unikt id -- colour-(färgnamn)
+    colourOptions.innerHTML = el;
+
+    const labelForColors = document.createElement("label");
+    labelForColors.setAttribute("for", colourOptions.id); // länka label till inputens id
+    labelForColors.textContent = el; // label texten blir färgnamen
+    labelForColors.innerHTML = el;
+
+    labelElement.appendChild(labelForColors);
+    labelElement.appendChild(colourOptions);
+};
