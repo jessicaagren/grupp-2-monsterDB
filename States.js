7d2c22c97777b3071eeebe35f5a2222de23756a0
Sylwia Przystawska <sylwiateresap@gmail.com>
2024-10-15 10:41:17 +0200

    Back to basic

--- States.js
+++
@@ -1,8 +0,0 @@
-// ===== STATE =====
-const allMonsters = [];
-
-// ===== CONFIG =====
-const monsterTypes = ["Ismonster", "Eldmonster", "Blixtmonster"];
-const monsterColours = ["röd", "rosa", "blå", "grön", "gul"];
-const monsterAttributes = ["huvuden", "tår", "armar", "horn"];
-const attributesContainer = {};
--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// ===== STATE =====
+const allMonsters = [];
+const attributesContainer = {};
+
+// ===== CONFIG =====
+const monsterTypes = ["Ismonster", "Eldmonster", "Blixtmonster"];
+const monsterColours = ["röd", "rosa", "blå", "grön", "gul"];
+const monsterAttributes = ["huvuden", "tår", "armar", "horn"];
+
+// ===== DOM HANDLES =====
+const typeSelect = document.querySelector("#monster-type");
+const colourSelect = document.querySelector("#monster-colour");
+const registerMonsterForm = document.querySelector("#monster-form");
+const allMonsterCards = document.querySelector("main");
+const monsterAttributeSpanElement =
+  document.querySelector("#monster-attribute");
+
 // TODO: vad gör detta stycket kod?
 for (const el of monsterTypes) {
   const typeOptions = document.createElement("option");
